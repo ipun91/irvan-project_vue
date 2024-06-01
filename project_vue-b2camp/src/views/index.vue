1ee2fca9469ddf9643950365edb34f501e0d3a60
ipun91 <irvanhermawan091@gmail.com>
2024-06-01 08:37:11 +0700

    tugas_landing_vue

---
+++ project_vue-b2camp/src/views/index.vue
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="icon" type="image/png" sizes="32x32" href="./assets/icons/favicon-32x32.png">
+    <title>B2Camp-Exam</title>
+</head>
+<body>
+
+    <header>
+        <img class="logo" src="./assets/img/logo_b2camp.png">
+        <nav>
+            <a class="menu" href="#sect-1">Home</a>
+            <a class="menu" href="#sect-2">Courses</a>
+            <a class="menu" href="#sect-3">Why B2Camp</a>
+            <a class="menu" href="#sect-4">Contact</a>
+        </nav>
+    </header>
+
+<!-- SECTION-1 -->
+
+    <section id="sect-1">
+        <div class="card-blu">
+            <h1 class="white">Improve Your Skills</h1>
+            <br>
+            <p>Bridge Bootcamp Indonesia adalah penyedia jasa pelatihan di bidang pemrograman untuk melahirkan programmer handal yang siap berkarir di bidang Informasi dan Teknologi. Berdiri sejak 01 Maret 2021.
+                B2Camp Indonesia untuk kita semua</p>
+            <br>
+            <div class="frem-btn">
+                <a class="btn-sec" href="#"><p>Get It Now!</p></a>
+                <a class="btn_text-sec" href="#"><p>Read more</p></a>
+            </div>
+        </div>
+    </section>
+
+<!-- SECTION-2 -->
+
+    <section id="sect-2">
+        <div class="card top-right">
+            <h1>Front-End Dev</h1>
+            <br>
+            <p>Jika Anda ingin mempelajari pengembangan web dan mendapatkan pekerjaan di industri teknologi,
+                Anda akan MENYUKAI kursus ini! Pelajari HTML, CSS, JavaScript, Bootstrap, dan lainnya dengan lebih dari 15 jam tutorial video HD! Kursus ini dirancang agar sangat ramah bagi pemula.
+                Kita akan mulai dengan dasar-dasar HTML dan membuat halaman web sederhana.
+                Di akhir kursus ini, Anda akan dapat mengembangkan dan mempublikasikan ekstensi Google Chrome Anda sendiri!</p>
+            <br>
+            <div class="frem-btn">
+                <a class="btn" href="#"><p>Get It Now!</p></a>
+                <a class="btn_text" href="#"><p>Read more</p></a>
+            </div>
+        </div>
+
+        <div class="card top-left">
+            <h1>Back-End Dev</h1>
+            <br>
+            <p>Rekayasa backend adalah sebuah seni. Selama 18 tahun karir saya bekerja dan membangun aplikasi backend,
+                saya menemukan bahwa pola desain komunikasi tertentu terus bermunculan.
+                Hanya ada sedikit cara klien berkomunikasi dengan aplikasi backend, meskipun mungkin lebih banyak lagi,
+                saya yakin pola yang saya bahas dalam kursus ini adalah yang paling umum.
+                Contoh pola ini adalah permintaan-respons, publikasikan-langganan, pendek dan panjang, serta push.</p>
+            <br>
+            <div class="frem-btn">
+                <a class="btn" href="#"><p>Get It Now!</p></a>
+                <a class="btn_text" href="#"><p>Read more</p></a>
+            </div>
+        </div>
+
+        <div class="card bottom-cent">
+            <h1>UI/UX Design</h1>
+            <br>
+            <p>Menggunakan praktik terbaik terbaru dalam Desain Web dan Desain Seluler serta Antarmuka Pengguna dan Desain Pengalaman Pengguna (UI/UX),
+                kursus ini berfokus untuk membawa Anda secara efisien dari nol ke titik di mana Anda dapat diterima bekerja atau memenangkan kontrak freelance.
+                Kami akan menggunakan alat yang banyak diminati seperti Figma untuk menunjukkan kepada Anda alur kerja lengkap dari awal hingga akhir.
+                Lulusan kursus Andrei kini bekerja di Google, Tesla, Amazon, Apple, IBM, JP Morgan, Facebook, + perusahaan teknologi terkemuka lainnya.</p>
+            <br>
+            <div class="frem-btn">
+                <a class="btn" href="#"><p>Get It Now!</p></a>
+                <a class="btn_text" href="#"><p>Read more</p></a>
+            </div>
+        </div>
+    </section>
+
+<!-- SECTION-3 -->
+
+    <section id="sect-3">
+        <h1 class="white">Kenapa harus B2Camp?</h1>
+        <div class="content-row">
+            <div class="card-product">
+                <img class="imgpro" src="./assets/img/prod1.jpg">
+                <div class="frem-text">
+                    <h6>Instruktur Profesional</h6>
+                    <p>Di B2Camp, kami percaya bahwa pendidikan terbaik datang dari para ahli.
+                        Tim pengajar kami terdiri dari instruktur profesional yang memiliki pengalaman luas di bidangnya.
+                        Mereka tidak hanya mengajar, tetapi juga membimbing dan menginspirasi setiap siswa untuk mencapai potensi maksimal mereka.</p>
+                </div>      
+            </div>
+    
+            <div class="card-product">
+                <img class="imgpro" src="./assets/img/prod2.jpg">
+                <div class="frem-text">
+                    <h6>Kurikulum Terdepan dan Terupdate</h6>
+                    <p>Kami selalu mengikuti perkembangan terbaru dalam dunia pendidikan dan memastikan kurikulum kami selalu relevan dan mutakhir.
+                        Materi yang kami ajarkan dirancang secara khusus untuk memenuhi kebutuhan pasar kerja saat ini dan masa depan, sehingga Anda selalu berada selangkah di depan.</p>
+                </div>      
+            </div>
+
+            <div class="card-product">
+                <img class="imgpro" src="./assets/img/prod3.webp">
+                <div class="frem-text">
+                    <h6>Proses Belajar yang Mendukung dan Inspiratif</h6>
+                    <p>Dalam bootcamp ini, sistem pengajarannya cukup baik dan selama mengikuti bootcamp ini,
+                        saya mendapatkan banyak ilmu yang baru mulai dari bahasa pemograman Java dan cara penggunaan GIT.
+                        Semoga kedepannya bootcamp ini menjadi lebih besar dan lebih baik lagi.</p>
+                </div>      
+            </div>
+    
+        </div>
+
+        <a class="btn-indi" href="#"><p>Get It Now!</p></a>
+
+    </section>
+    
+<!-- SECTION-4 -->
+
+    <section id="sect-4">
+
+        <h1 class="blue">Contact Us</h1>
+        <div class="content-col">
+            <div class="list">
+                <img src="./assets/icons/phone.svg">
+                <p>+62 812 3456 7890</p>
+            </div>
+
+            <div class="list">
+                <img src="./assets/icons/mail.svg">
+                <p>b2camp@learning.id</p>
+            </div>
+
+            <div class="list">
+                <img src="./assets/icons/pin-loc.svg">
+                <p>Bandung, West Java, Indonesia</p>
+            </div>
+        </div>
+
+    </section>
+
+    <footer>
+        <p>Copyright 2020-2024. All Rights Reserved. B2Camp.</p>
+    </footer>
+    
+</body>
+</html>
